ead108be095a4defb2b6dc2a7783339ff96be702
Mahfuj <mahfujurrahman2077@gmail.com>
2025-09-16 03:09:10 +0600

    practice 09

--- 06 Nested loop/09.c
+++ 06 Nested loop/09.c
@@ -5,10 +5,9 @@ int main() {
     printf("Enter a number: ");
     scanf("%d", &n);
 
-
-    for(int i = 0; i < n; i++) {
-        for(int j = 0; j < n; j++) {
-            printf("*");
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("%d", (i + j) % 2);
         }
         printf("\n");
     }
